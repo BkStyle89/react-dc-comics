5cfa51b04fe72adaf128aefe16f3249e76e68295
BkStyle89 <kajzer@hotmail.it>
2026-03-06 16:26:22 +0100

    header buttons

--- REACT-DC-COMICS/src/components/AppHeader.jsx
+++ REACT-DC-COMICS/src/components/AppHeader.jsx
@@ -3,7 +3,23 @@ export default function AppHeader(){
 
   return (
     <header> 
-      header
+      <div className="container">
+        <div className="row">
+          <img src="../assets/img/dc-logo.png" alt="" />
+          <div>
+            <button>CHARACTERS</button>
+            <button>COMICS</button>
+            <button>MOVIES</button>
+            <button>TV</button>
+            <button>GAMES</button>
+            <button>COLLECTIBLES</button>
+            <button>VIDEOS</button>
+            <button>FANS</button>
+            <button>NEWS</button>
+            <button>SHOP</button>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
