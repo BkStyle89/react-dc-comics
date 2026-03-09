bc9c54c93c078d6d466bf67bfa48d792491cb9dc
BkStyle89 <kajzer@hotmail.it>
2026-03-09 11:56:02 +0100

    more correction and jumbotron responsice

--- REACT-DC-COMICS/src/components/AppHeader.jsx
+++ REACT-DC-COMICS/src/components/AppHeader.jsx
@@ -7,23 +7,23 @@ export default function AppHeader(){
       <div className="container">
         <div className="row">
           <div id='upBar'>
-          <img className='d-none d-md-block' id='logo' src={logo} alt="DC-LOGO" />
-            <div className='navigation'>
-              <a className='navBar p-2' href="">CHARACTERS</a>
-              <a className='navBar p-2' href="">COMICS</a>
-              <a className='navBar p-2' href="">MOVIES</a>
-              <a className='navBar p-2' href="">TV</a>
-              <a className='navBar p-2' href="">GAMES</a>
-              <a className='navBar p-2' href="">COLLECTIBLES</a>
-              <a className='navBar p-2' href="">VIDEOS</a>
-              <a className='navBar p-2' href="">FANS</a>
-              <a className='navBar p-2' href="">NEWS</a>
-              <a className='navBar p-2' href="">SHOP</a>
+          <img className='d-none d-lg-block' id='logo' src={logo} alt="DC-LOGO" />
+            <div className='navigation '>
+              <a className='navBar p-1' href="">CHARACTERS</a>
+              <a className='navBar p-1' href="">COMICS</a>
+              <a className='navBar p-1' href="">MOVIES</a>
+              <a className='navBar p-1' href="">TV</a>
+              <a className='navBar p-1' href="">GAMES</a>
+              <a className='navBar p-1' href="">COLLECTIBLES</a>
+              <a className='navBar p-1' href="">VIDEOS</a>
+              <a className='navBar p-1' href="">FANS</a>
+              <a className='navBar p-1' href="">NEWS</a>
+              <a className='navBar p-1' href="">SHOP</a>
             </div>
           </div>
         </div>
       </div>
-      <div id='jumbotron'>
+      <div className='d-none d-md-block' id='jumbotron'>
       </div>
     </header>
   )
